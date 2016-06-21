0c4dafab8a3550dcc3e0646e301b5c70645f9538
me <me@here.com>
2016-06-21 16:56:59 +0000

    syntax error

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,13 @@
-
+# Configure SSH Daemon
 class ssh::config {
-	file { '/etc/ssh/sshd_config' :
-		ensure 	=> 'present',
-		owner 	=> 'root',
-		group 	=> 'root',
-		mode 	=> '0400',
-		source 	=> 'puppet:///modules/ssh/sshd_config',
-		require => Package['openssh-server'],
-	}
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'puppet:///modules/ssh/sshd_config',
+    require => Package['openssh-server'],
+  }
+
 }
