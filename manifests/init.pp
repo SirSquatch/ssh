d9a57fcfcc9b16adfdf154bcf37a286172907b8f
me <me@here.com>
2016-06-21 17:01:32 +0000

    syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,8 +43,8 @@
 # Copyright 2016 Your name here, unless otherwise noted.
 #
 class ssh {
-	include config
-	include install
-	include service
 
+  include [ '::ssh::config' ]
+  include [ '::ssh::install' ]
+  include [ '::ssh::service' ]
 }
